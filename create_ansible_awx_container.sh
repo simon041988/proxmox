d3a503281f1cfe95d6840409de2fee61727394ea
simon041988 <42072768+simon041988@users.noreply.github.com>
2023-05-06 21:02:41 +0200

    Update create_ansible_awx_container.sh

--- create_ansible_awx_container.sh
+++ create_ansible_awx_container.sh
@@ -2,24 +2,25 @@
 
 # Define variables for container creation
 CONTAINER_NAME="ansible-awx"
-VM_ID="9000"
+VM_ID=114
 VM_TYPE="lxc"
 OS_TEMPLATE="local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst"
 ROOTFS_SIZE="10G"
-CPU_CORES="2"
-MEMORY="4096"
-SWAP="1024"
+CPU_CORES=2
+MEMORY=4096
+SWAP=1024
 SSH_PUBLIC_KEY="/root/.ssh/id_rsa.pub"
-SSH_PORT="22"
+SSH_PORT=22
 IP_ADDRESS="192.168.2.27"
 GATEWAY="192.168.2.1"
 DNS_SERVER="192.168.2.11"
 
 # Create the Proxmox LXC container
-qm create $VM_ID -name $CONTAINER_NAME -net0 "name=eth0,bridge=vmbr0,ip=$IP_ADDRESS/24,gw=$GATEWAY" -ostype $OS_TEMPLATE -rootfs $ROOTFS_SIZE -memory $MEMORY -swap $SWAP -cpu $CPU_CORES -sshkey $SSH_PUBLIC_KEY -sshport $SSH_PORT -onboot 1 -startup 1 -searchdomain $DNS_SERVER -agent 1 -hookscript /usr/share/doc/vzdump/examples/hookscript.pl -lock backup
+qm create $VM_ID --name $CONTAINER_NAME --net0 "name=eth0,bridge=vmbr0,ip=$IP_ADDRESS/24,gw=$GATEWAY" --ostype $OS_TEMPLATE --rootfs $ROOTFS_SIZE --memory $MEMORY --swap $SWAP --cores $CPU_CORES --sshkey $SSH_PUBLIC_KEY --ssh-port $SSH_PORT --onboot 1 --startup 1 --searchdomain $DNS_SERVER --agent 1 --hookscript /usr/share/doc/vzdump/examples/hookscript.pl --lock backup
 
 # Start the container
 qm start $VM_ID
+
 
 # Warten, bis der Container gestartet ist
 sleep 10
