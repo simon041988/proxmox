4300295f1fac2ab20bd11778a9abc440e0dd25a8
simon041988 <42072768+simon041988@users.noreply.github.com>
2023-05-06 21:09:22 +0200

    Update create_ansible_awx_container.sh

--- create_ansible_awx_container.sh
+++ create_ansible_awx_container.sh
@@ -1,45 +1,32 @@
 #!/bin/bash
 
-# Define variables for container creation
-CONTAINER_NAME="ansible-awx"
-VM_ID=114
-VM_TYPE="lxc"
-OS_TEMPLATE="local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst"
-ROOTFS_SIZE="10G"
-CPU_CORES=2
-MEMORY=4096
-SWAP=1024
-SSH_PUBLIC_KEY="/root/.ssh/id_rsa.pub"
-SSH_PORT=22
-IP_ADDRESS="192.168.2.27"
-GATEWAY="192.168.2.1"
-DNS_SERVER="192.168.2.11"
+# Prompt for root password
+echo "Please enter a root password for the container:"
+read -s rootpw
 
-# Create the Proxmox LXC container
-pct create $VM_ID $OS_TEMPLATE --hostname $CONTAINER_NAME --memory $MEMORY --swap $SWAP --cpu $CPU_CORES --net0 name=eth0,bridge=vmbr0,ip=$IP_ADDRESS/24,gw=$GATEWAY --ssh-public-keys $SSH_PUBLIC_KEY --onboot 1 --ostype Debian --searchdomain $DNS_SERVER
+# Create the container
+vmid=114
+hostname=awx
+ostemplate=debian-11-standard_11.6-1_amd64.tar.zst
+rootfs=10
+swap=1024
+cpus=2
+memory=4096
+net0="name=eth0,bridge=vmbr0,ip=192.168.2.27/24,gw=192.168.2.1"
+sshport=2222
 
-# Set root password for container
-echo "password123" | pct set $VM_ID --root-password
+pct create $vmid $ostemplate -rootfs $rootfs -swap $swap -hostname $hostname -cpus $cpus -memory $memory -net0 $net0 -ssh-public-keys /root/.ssh/id_rsa.pub -onboot 1 -startup order=1
+pct set $vmid -root-password "$rootpw" -ssh-port $sshport
+pct start $vmid
 
-# Start the container
-pct start $VM_ID
-
-
-
-# Warten, bis der Container gestartet ist
+# Wait for the container to start up
 sleep 10
 
-# Ansible Core installieren
-pct exec $VMID -- bash -c "apt-get update && apt-get install -y ansible"
+# Install Ansible Core and AWX
+pct exec $vmid -- sh -c 'echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list && apt-get update && apt-get -y install ansible'
+pct exec $vmid -- sh -c 'apt-get -y install curl gnupg2 software-properties-common && curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" && apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io'
+pct exec $vmid -- sh -c 'curl -s https://api.github.com/repos/ansible/awx/archive/refs/tags/17.0.0.tar.gz | tar -xvz && cd awx-17.0.0/installer/ && ansible-playbook -i inventory install.yml'
 
-# AWX installieren
-pct exec $VMID -- bash -c "apt-get install -y gnupg2 curl && curl -s https://packagecloud.io/install/repositories/ansible/awx/script.deb.sh | bash && apt-get install -y ansible-awx"
-
-# AWX-Konfiguration
-pct exec $VMID -- bash -c "cd /var/lib/awx/installer && ansible-playbook -i inventory install.yml"
-
-# AWX starten
-pct exec $VMID -- bash -c "systemctl start docker-compose@awx"
-
-# AWX bei Systemstart starten
-pct exec $VMID -- bash -c "systemctl enable docker-compose@awx"
+# Print login information
+echo "AWX should now be installed and accessible at https://192.168.2.100 with your root password."
+echo "SSH access is available on port $sshport with username 'root' and your root password." 
