c60a2e8ec3fbd1db33a00c618fa099aa8aad7986
simon041988 <42072768+simon041988@users.noreply.github.com>
2023-05-06 21:13:48 +0200

    Update create_ansible_awx_container.sh

--- create_ansible_awx_container.sh
+++ create_ansible_awx_container.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-# Prompt for root password
-echo "Please enter a root password for the container:"
-read -s rootpw
+# Define variables
+VMID=114
+OSTEMPLATE=local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst
+DISKSIZE=10G
+SWAPSIZE=1024
+CPUS=2
+MEMORY=4096
+IPADDRESS=192.168.2.27
+GATEWAY=192.168.2.1
+DNS=192.168.2.11
 
-# Create the container
-vmid=114
-hostname=awx
-ostemplate=debian-11-standard_11.6-1_amd64.tar.zst
-rootfs=10
-swap=1024
-cpus=2
-memory=4096
-net0="name=eth0,bridge=vmbr0,ip=192.168.2.27/24,gw=192.168.2.1"
-sshport=2222
+# Create LXC container
+pct create $VMID $OSTEMPLATE -rootfs $DISKSIZE -swap $SWAPSIZE -cpus $CPUS -memory $MEMORY -net0 name=eth0,ip=$IPADDRESS/24,gw=$GATEWAY -nameserver $DNS
 
-pct create $vmid $ostemplate -rootfs $rootfs -swap $swap -hostname $hostname -cpus $cpus -memory $memory -net0 $net0 -ssh-public-keys /root/.ssh/id_rsa.pub -onboot 1 -startup order=1
-pct set $vmid -root-password "$rootpw" -ssh-port $sshport
-pct start $vmid
+# Set hostname
+pct set $VMID -hostname ansible
 
-# Wait for the container to start up
+# Set root password interactively
+pct set $VMID -rootfs password
+
+# Set SSH port
+pct set $VMID -ssh-port 2222
+
+# Start container
+pct start $VMID
+
+# Wait for container to start
 sleep 10
+
+# Install sshpass to be able to execute ssh commands with password authentication
+pct exec $VMID -- apt update
+pct exec $VMID -- apt install -y sshpass
+
 
 # Install Ansible Core and AWX
 pct exec $vmid -- sh -c 'echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list && apt-get update && apt-get -y install ansible'
